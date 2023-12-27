93d1f846662941e3684f6c8fd67079decc922f3a
Shashank Naik <shashanknaik808@gmail.com>
2023-12-27 15:56:49 +0530

    Refactor AddBlogs component to functional with hooks, and clean up code

--- src/components/AddBlogs.jsx
+++ src/components/AddBlogs.jsx
@@ -1,145 +1,164 @@
+import React, { useState } from 'react';
 import axios from 'axios';
-import React, { Component } from 'react'
-// import { Link } from 'react-router-dom'
 
-export class AddBlogs extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            inputs: {
-                title: '',
-                description: '',
-                image: ''
-            },
-            error: {
-                errFlag: false,
-                errStatus: "",
-                errMsg: ""
-            }
-        }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+function AddBlogs() {
+    const [state, setState] = useState({
+        inputs: {
+            title: '',
+            description: '',
+            image: '',
+        },
+        error: {
+            errFlag: false,
+            errStatus: '',
+            errMsg: '',
+        },
+    });
 
-    handleChange(e) {
-        this.setState(prevState => {
-
+    function handleChange(e) {
+        setState((prevState) => {
             if (!e.target.files || Object.keys(e.target.files).length === 0) {
-                return ({
+                return {
                     ...prevState,
                     inputs: {
                         ...prevState.inputs,
                         [e.target.name]: e.target.value,
-                    }
-                })
+                    },
+                };
             }
 
-            return ({
+            return {
                 ...prevState,
                 inputs: {
                     ...prevState.inputs,
                     [e.target.name]: e.target.value,
-                    image: e.target.files[0]
-                }
-            });
+                    image: e.target.files[0],
+                },
+            };
         });
     }
-    async sendRequest() {
-        const formData = new FormData();
-        formData.append('title', this.state.inputs.title);
-        formData.append('description', this.state.inputs.description);
-        formData.append('userID', localStorage.getItem("userID"));
-        formData.append('image', this.state.inputs.image, this.state.inputs.image.name);
 
-        let res;
+    async function sendRequest() {
+        const formData = new FormData();
+        formData.append('title', state.inputs.title);
+        formData.append('description', state.inputs.description);
+        formData.append('userID', localStorage.getItem('userID'));
+        formData.append('image', state.inputs.image, state.inputs.image.name);
 
         try {
-            res = await axios.post(`http://localhost:5000/api/blog/add`, formData)
-        }
-        catch (err) {
-            this.setState(prevState => ({
+            const res = await axios.post(
+                'http://localhost:5000/api/blog/add',
+                formData
+            );
+            return res.data;
+        } catch (err) {
+            setState((prevState) => ({
                 ...prevState,
                 error: {
                     errFlag: true,
                     errStatus: err.response.request.status,
                     errMsg: err.response.data.message,
-                }
+                },
             }));
+            return null;
         }
-
-        let data = null;
-        if (res) {
-            data = await res.data;
-        }
-        return data;
     }
 
-    handleSubmit(e) {
+    function handleSubmit(e) {
         e.preventDefault();
-        this.sendRequest()
-            .then(data => {
-                window.location.replace("/myBlogs")
-            })
-
+        sendRequest().then((data) => {
+            if (data) {
+                window.location.replace('/myBlogs');
+            }
+        });
     }
-    render() {
 
-        return (
-            <>
-                <header className="masthead" style={{ "backgroundImage": "url('xxxxx')" }}>
-                    <div className="container position-relative px-4 px-lg-5">
-                        <div className="row gx-4 gx-lg-5 justify-content-center">
-                            <div className="col-md-10 col-lg-8 col-xl-7">
-                                <div className="page-heading">
-                                    <h1>{this.state.inputs.title}</h1>
-                                    <span className="subheading">{this.state.inputs.description}</span>
-                                </div>
+    return (
+        <>
+            <header className="masthead" style={{ backgroundImage: "url('xxxxx')" }}>
+                <div className="container position-relative px-4 px-lg-5">
+                    <div className="row gx-4 gx-lg-5 justify-content-center">
+                        <div className="col-md-10 col-lg-8 col-xl-7">
+                            <div className="page-heading">
+                                <h1>{state.inputs.title}</h1>
+                                <span className="subheading">{state.inputs.description}</span>
                             </div>
                         </div>
                     </div>
-                </header>
+                </div>
+            </header>
 
-                <div className="container px-4 px-lg-5">
-                    <div className="row gx-4 gx-lg-5 justify-content-center">
-                        <div className="col-md-10 col-lg-8 col-xl-7">
-                            <form action="/blogs" method="POST" onSubmit={this.handleSubmit} >
-                                <div className="form-floating">
-                                    <input className="form-control" id="title" name="title" type="text"
-                                        placeholder="Enter the title..." onChange={this.handleChange} required />
-                                    <label htmlFor="title">Title</label>
-                                </div>
+            <div className="container px-4 px-lg-5">
+                <div className="row gx-4 gx-lg-5 justify-content-center">
+                    <div className="col-md-10 col-lg-8 col-xl-7">
+                        <form action="/blogs" method="POST" onSubmit={handleSubmit}>
+                            <div className="form-floating">
+                                <input
+                                    className="form-control"
+                                    id="title"
+                                    name="title"
+                                    type="text"
+                                    placeholder="Enter the title..."
+                                    onChange={handleChange}
+                                    required
+                                />
+                                <label htmlFor="title">Title</label>
+                            </div>
 
-                                <div className="form-floating">
-                                    <textarea className="form-control" id="description" name="description"
-                                        placeholder="Enter your description here..." onChange={this.handleChange} required></textarea>
-                                    <label htmlFor="description">Description</label>
-                                </div>
+                            <div className="form-floating">
+                                <textarea
+                                    className="form-control"
+                                    id="description"
+                                    name="description"
+                                    placeholder="Enter your description here..."
+                                    onChange={handleChange}
+                                    required
+                                ></textarea>
+                                <label htmlFor="description">Description</label>
+                            </div>
 
-                                <div className="form-floating">
-                                    <input className="form-control" id="image" name="image" type="file"
-                                        placeholder="Upload an Image" onChange={this.handleChange} required></input>
-                                    <label htmlFor="image">Image</label>
-                                </div><br />
+                            <div className="form-floating">
+                                <input
+                                    className="form-control"
+                                    id="image"
+                                    name="image"
+                                    type="file"
+                                    placeholder="Upload an Image"
+                                    onChange={handleChange}
+                                    required
+                                ></input>
+                                <label htmlFor="image">Image</label>
+                            </div>
+                            <br />
 
-                                {/* Submit Button*/}
-                                <div style={{ textAlign: 'center' }}>
-                                    <button id="submitButton" className="btn btn-primary text-uppercase" type="submit">Send</button>
-                                </div>
+                            <div style={{ textAlign: 'center' }}>
+                                <button
+                                    id="submitButton"
+                                    className="btn btn-primary text-uppercase"
+                                    type="submit"
+                                >
+                                    Send
+                                </button>
+                            </div>
 
-                                {
-                                    (this.state.error.errFlag) &&
-                                    <span style={{ 'color': '#dc3545', 'fontWeight': 'bold', 'fontStyle': 'oblique' }}>
-                                        &ensp; &ensp; {this.state.error.errMsg} &ensp; &ensp; :  {this.state.error.errStatus}
-                                    </span>
-                                }
-                            </form>
-                        </div>
+                            {state.error.errFlag && (
+                                <span
+                                    style={{
+                                        color: '#dc3545',
+                                        fontWeight: 'bold',
+                                        fontStyle: 'oblique',
+                                    }}
+                                >
+                                    &ensp; &ensp; {state.error.errMsg} &ensp; &ensp; :{' '}
+                                    {state.error.errStatus}
+                                </span>
+                            )}
+                        </form>
                     </div>
                 </div>
-            </>
-
-        )
-    }
+            </div>
+        </>
+    );
 }
 
 export default AddBlogs;
