192bc413623b4d17baf1f3f7e693d76dd6532e04
Shashank Naik <shashanknaik808@gmail.com>
2023-12-27 16:20:55 +0530

    Refactor Header component to functional component with destructuring

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export class Header extends Component {
+function Header({ banner, heading, subHeading }) {
+    let imageURL = `url('${banner}')`;
 
-    render() {
-
-        let imageURL = `url('${this.props.banner}')`;
-        console.log(imageURL);
-
-        return (
-            <>
-                <header className="masthead" style={{ "backgroundImage": { imageURL } }}>                    <div className="container position-relative px-4 px-lg-5">
+    return (
+        <>
+            <header className="masthead" style={{ backgroundImage: imageURL }}>
+                <div className="container position-relative px-4 px-lg-5">
                     <div className="row gx-4 gx-lg-5 justify-content-center">
                         <div className="col-md-10 col-lg-8 col-xl-7">
                             <div className="page-heading">
-                                <h1>{this.props.heading}</h1>
-                                <span className="subheading">{this.props.subHeading}</span>
+                                <h1>{heading}</h1>
+                                <span className="subheading">{subHeading}</span>
                             </div>
                         </div>
                     </div>
                 </div>
-                </header>
-            </>
-        )
-    }
+            </header>
+        </>
+    );
 }
 
 export default Header;
