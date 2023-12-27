b9cc0355b88f86da1a30a81b87e61c5d9d853fad
Shashank Naik <shashanknaik808@gmail.com>
2023-12-27 16:12:17 +0530

    Refactor BlogDetail component to functional with hooks, improve code structure

--- src/components/BlogDetail.jsx
+++ src/components/BlogDetail.jsx
@@ -1,153 +1,173 @@
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import React, { Component } from 'react'
 
-export class BlogDetail extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            inputs: {
-                title: "",
-                description: "",
-                image: ""
-            }
+function BlogDetail() {
+    const [inputs, setInputs] = useState({
+        title: '',
+        description: '',
+        image: '',
+    });
+
+    async function fetchDetails(id) {
+        try {
+            const res = await axios.get(`http://localhost:5000/api/blog/${id}`);
+            const data = res.data;
+            return data;
+        } catch (err) {
+            console.log('Data not getting', err);
+            return null;
         }
-        this.sendRequest = this.sendRequest.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.fetchDetails = this.fetchDetails.bind(this);
     }
 
-    handleChange(e) {
-        this.setState(prevState => {
+    useEffect(() => {
+        const fetchData = async () => {
+            const idArr = document.URL.split('/');
+            let id = idArr[idArr.length - 1];
 
-            if (!e.target.files || Object.keys(e.target.files).length === 0) {
-                return ({
-                    inputs: {
-                        ...prevState.inputs,
-                        [e.target.name]: e.target.value,
-                    }
-                })
-            }
-
-            return ({
-                inputs: {
-                    ...prevState.inputs,
-                    [e.target.name]: e.target.value,
-                    image: e.target.files[0]
-                }
-            });
-        });
-    }
-
-    async fetchDetails(id) {
-        const res = await axios.get(`http://localhost:5000/api/blog/${id}`)
-            .catch(err => console.log("Data not getting"))
-
-        let data = null;
-        if (res) {
-            data = await res.data;
-            console.log(data);
-        }
-        return data;
-    }
-
-    async componentDidMount() {
-        const idArr = document.URL.split('/');
-        let id = idArr[idArr.length - 1];
-
-        this.fetchDetails(id)
-            .then(data => this.setState({
-                inputs: {
+            const data = await fetchDetails(id);
+            if (data) {
+                setInputs({
                     title: data.blog.title,
                     description: data.blog.description,
                     image: data.blog.image,
-                }
-            }))
-    }
+                });
+            }
+        };
 
-    async sendRequest(id) {
+        fetchData();
+    }, []);
+
+    async function sendRequest(id) {
         const formData = new FormData();
-        formData.append('title', this.state.inputs.title);
-        formData.append('description', this.state.inputs.description);
-        formData.append('image', this.state.inputs.image, this.state.inputs.image.name);
+        formData.append('title', inputs.title);
+        formData.append('description', inputs.description);
+        formData.append('image', inputs.image, inputs.image.name);
 
-        const res = await axios.put(`http://localhost:5000/api/blog/update/${id}`, formData)
-            .catch(err => console.log(err))
-
-        let data = null;
-        if (res) {
-            data = await res.data;
+        try {
+            const res = await axios.put(
+                `http://localhost:5000/api/blog/update/${id}`,
+                formData
+            );
+            const data = res.data;
             console.log(data);
+            return data;
+        } catch (err) {
+            console.log(err);
+            return null;
         }
-        return data;
     }
 
-    handleSubmit(e) {
+    const handleChange = (e) => {
+        setInputs((prevInputs) => {
+            if (!e.target.files || Object.keys(e.target.files).length === 0) {
+                return {
+                    ...prevInputs,
+                    [e.target.name]: e.target.value,
+                };
+            }
+
+            return {
+                ...prevInputs,
+                [e.target.name]: e.target.value,
+                image: e.target.files[0],
+            };
+        });
+    };
+
+    const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state.inputs);
-        // console.log(this.state.blog);
         const idArr = document.URL.split('/');
         let id = idArr[idArr.length - 1];
-        this.sendRequest(id)
-            .then(data => console.log(data))
+        sendRequest(id)
+            .then((data) => console.log(data))
             .then(() => {
-                window.location.replace("/myBlogs")
+                window.location.replace('/myBlogs');
             })
-            .catch(() => console.log("Can not update"))
-    }
+            .catch(() => console.log('Can not update'));
+    };
 
+    return (
+        <>
+            <header
+                className="masthead"
+                style={{ backgroundImage: `url('http://localhost:5000${inputs.image}')` }}
+            >
+                <div className="container position-relative px-4 px-lg-5">
+                    <div className="row gx-4 gx-lg-5 justify-content-center">
+                        <div className="col-md-10 col-lg-8 col-xl-7">
+                            <div className="page-heading">
+                                <h1>{inputs.title}</h1>
+                                <span className="subheading">{inputs.description}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
 
-    render() {
-        return (
-            <>
-                <header className="masthead" style={{ "backgroundImage": "url('http://localhost:5000" + this.state.inputs.image + "')" }}>
-                    <div className="container position-relative px-4 px-lg-5">
-                        <div className="row gx-4 gx-lg-5 justify-content-center">
-                            <div className="col-md-10 col-lg-8 col-xl-7">
-                                <div className="page-heading">
-                                    <h1>{this.state.inputs.title}</h1>
-                                    <span className="subheading">{this.state.inputs.description}</span>
+            {/* Post Content*/}
+            {inputs && (
+                <div className="container px-4 px-lg-5">
+                    <div className="row gx-4 gx-lg-5 justify-content-center">
+                        <div className="col-md-10 col-lg-8 col-xl-7">
+                            <form action="/blogs" method="POST" onSubmit={handleSubmit}>
+                                <div className="form-floating">
+                                    <input
+                                        className="form-control"
+                                        id="title"
+                                        name="title"
+                                        type="text"
+                                        placeholder="Enter the title..."
+                                        value={inputs.title}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                    <label htmlFor="title">Title</label>
                                 </div>
-                            </div>
+
+                                <div className="form-floating">
+                                    <textarea
+                                        className="form-control"
+                                        id="description"
+                                        name="description"
+                                        placeholder="Enter your description here..."
+                                        value={inputs.description}
+                                        onChange={handleChange}
+                                        required
+                                    ></textarea>
+                                    <label htmlFor="description">Description</label>
+                                </div>
+                                <div className="form-floating">
+                                    <input
+                                        className="form-control"
+                                        id="image"
+                                        name="image"
+                                        type="file"
+                                        placeholder="Upload an Image"
+                                        onChange={handleChange}
+                                        required
+                                    ></input>
+                                    <label htmlFor="image">Image</label>
+                                </div>
+                                <br />
+
+                                {/* Submit Button*/}
+                                <div style={{ textAlign: 'center' }}>
+                                    <button
+                                        id="submitButton"
+                                        className="btn btn-primary text-uppercase"
+                                        type="submit"
+                                    >
+                                        Update
+                                    </button>
+                                </div>
+                                <br />
+                            </form>
                         </div>
                     </div>
-                </header>
-
-                {/* Post Content*/}
-                {this.state.inputs &&
-                    <div className="container px-4 px-lg-5">
-                        <div className="row gx-4 gx-lg-5 justify-content-center">
-                            <div className="col-md-10 col-lg-8 col-xl-7">
-                                <form action="/blogs" method="POST" onSubmit={this.handleSubmit} >
-                                    <div className="form-floating">
-                                        <input className="form-control" id="title" name="title" type="text"
-                                            placeholder="Enter the title..." value={this.state.inputs.title} onChange={this.handleChange} required />
-                                        <label htmlFor="title">Title</label>
-                                    </div>
-
-                                    <div className="form-floating">
-                                        <textarea className="form-control" id="description" name="description"
-                                            placeholder="Enter your description here..." value={this.state.inputs.description} onChange={this.handleChange} required></textarea>
-                                        <label htmlFor="description">Description</label>
-                                    </div>
-                                    <div className="form-floating">
-                                        <input className="form-control" id="image" name="image" type="file"
-                                            placeholder="Upload an Image" onChange={this.handleChange} required></input>
-                                        <label htmlFor="image">Image</label>
-                                    </div><br />
-
-                                    {/* Submit Button*/}
-                                    <div style={{ textAlign: 'center' }}>
-                                        <button id="submitButton" className="btn btn-primary text-uppercase" type="submit">Update</button>
-                                    </div><br />
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                }
-            </>
-        )
-    }
+                </div>
+            )}
+        </>
+    );
 }
 
 export default BlogDetail;
