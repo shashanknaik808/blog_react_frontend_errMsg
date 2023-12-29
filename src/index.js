0411fd888923d688df4740104c6c09d313068cba
Shashank Naik <shashanknaik808@gmail.com>
2023-12-29 12:26:49 +0530

    Remove dark mode toggle

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
-// index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </BrowserRouter>
 );
