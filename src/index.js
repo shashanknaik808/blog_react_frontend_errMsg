4bab455a6d44c2f14aac7ba5f7330da0915a701f
Shashank Naik <shashanknaik808@gmail.com>
2023-12-29 12:17:03 +0530

    Integrate MUI dark theme in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>
 );
