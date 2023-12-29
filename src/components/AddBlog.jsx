9247edbcc53b3c11934bbd4c072e28e8657fe863
Shashank Naik <shashanknaik808@gmail.com>
2023-12-29 12:32:31 +0530

    Refactor AddBlog component, introduce state for input fields and validation errors

--- src/components/AddBlog.jsx
+++ src/components/AddBlog.jsx
@@ -1,43 +1,83 @@
+import React, { useState } from 'react';
 import { Box, Button, InputLabel, TextField, Typography } from '@mui/material';
 import axios from 'axios';
-import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useStyles } from './utils.jsx';
 
-const labelStyle = { mb: 1, mt: 2, fontSize: '24px', fontWeight: 'bold' }
+const labelStyle = { mb: 1, mt: 2, fontSize: '24px', fontWeight: 'bold' };
 
 function AddBlog() {
-
     const classes = useStyles();
     const navigate = useNavigate();
 
     const [inputs, setInputs] = useState({
-        title: "", description: "", imageURL: ""
-    })
+        title: "",
+        description: "",
+        imageURL: ""
+    });
+
+    const [errors, setErrors] = useState({
+        titleError: "",
+        descriptionError: "",
+        imageURLError: ""
+    });
 
     function handleChange(e) {
         setInputs((prevState) => ({
             ...prevState,
             [e.target.name]: e.target.value
-        })
-        );
+        }));
+
+        setErrors((prevErrors) => ({
+            ...prevErrors,
+            titleError: "",
+            descriptionError: "",
+            imageURLError: ""
+        }));
     }
+
     async function sendRequest() {
-        const res = await axios.post(`http://localhost:5000/api/blog/add`, {
-            title: inputs.title,
-            description: inputs.description,
-            image: inputs.imageURL,
-            user: localStorage.getItem("userID")
-        }).catch(err => console.log(err))
-        const data = await res.data;
-        return data;
+        try {
+            const res = await axios.post(`http://localhost:5000/api/blog/add`, {
+                title: inputs.title,
+                description: inputs.description,
+                image: inputs.imageURL,
+                user: localStorage.getItem("userID")
+            });
+
+            const data = await res.data;
+            return data;
+        } catch (err) {
+            let errors = [];
+
+            if (err.response?.status === 400) {
+                errors.push("Validation error");
+            }
+
+            if (errors.length === 0) {
+                errors.push("Internal server error");
+            }
+
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                titleError: errors.includes("Validation error") ? "Validation error" : "",
+                descriptionError: errors.includes("Validation error") ? "Validation error" : "",
+                imageURLError: errors.includes("Validation error") ? "Validation error" : "",
+            }));
+
+            throw err;
+        }
     }
+
     function handleSubmit(e) {
         e.preventDefault();
-        console.log(inputs);
-        sendRequest().then(data => console.log(data))
+
+        sendRequest()
+            .then(data => console.log(data))
             .then(() => navigate("/blogs"))
+            .catch(error => console.error("Error:", error));
     }
+
     return (
         <div>
             <form onSubmit={handleSubmit}>
@@ -61,16 +101,40 @@ function AddBlog() {
                         variant='h2'
                         textAlign='center'>Post Your Blog</Typography>
                     <InputLabel className={classes.font} sx={labelStyle}>Title</InputLabel>
-                    <TextField name='title' onChange={handleChange} value={inputs.title} margin='auto' variant='outlined' />
+                    <TextField
+                        name='title'
+                        onChange={handleChange}
+                        value={inputs.title}
+                        margin='auto'
+                        variant='outlined'
+                        error={!!errors.titleError}
+                        helperText={errors.titleError}
+                    />
                     <InputLabel className={classes.font} sx={labelStyle}>Description</InputLabel>
-                    <TextField name='description' onChange={handleChange} value={inputs.description} margin='auto' variant='outlined' />
+                    <TextField
+                        name='description'
+                        onChange={handleChange}
+                        value={inputs.description}
+                        margin='auto'
+                        variant='outlined'
+                        error={!!errors.descriptionError}
+                        helperText={errors.descriptionError}
+                    />
                     <InputLabel className={classes.font} sx={labelStyle}>Image</InputLabel>
-                    <TextField name='imageURL' onChange={handleChange} value={inputs.imageURL} margin='auto' variant='outlined' />
+                    <TextField
+                        name='imageURL'
+                        onChange={handleChange}
+                        value={inputs.imageURL}
+                        margin='auto'
+                        variant='outlined'
+                        error={!!errors.imageURLError}
+                        helperText={errors.imageURLError}
+                    />
                     <Button type="submit" sx={{ mt: 2, borderRadius: 4 }} variant='contained' color='warning'>Submit</Button>
                 </Box>
             </form>
         </div>
-    )
+    );
 }
 
 export default AddBlog;
