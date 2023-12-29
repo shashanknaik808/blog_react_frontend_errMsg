1240dd99f3dce71231828af7d849e117d39f685d
Shashank Naik <shashanknaik808@gmail.com>
2023-12-29 11:54:13 +0530

    Restarting the Project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import Sample from './Sample';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <Sample />
+    <App />
   </BrowserRouter>
 );
